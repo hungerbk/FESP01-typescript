f9bf9e659eaaf30908bbf70944d49a1fd75dc9b4
hungerbk <wjantlr@gmail.com>
2023-11-01 21:56:45 +0900

    인터페이스로 객체의 타입 선언

--- workspace/ch01/ex01-09.ts
+++ workspace/ch01/ex01-09.ts
@@ -1,5 +1,20 @@
 // 인터페이스
 // ex01-07.ts 복사
-(()=>{
-  
+(() => {
+  // 객체의 타입 선언
+  interface User {
+    name: string;
+    age: number;
+  }
+  // 객체 생성
+  var kim: User = { name: "김철수", age: 30 };
+
+  console.log(kim);
+
+  function printUser(user: User): void {
+    // void이기 때문에 함수의 리턴값을 변수에 할당할 수 없음 (리턴값이 없음)
+    console.log(user.name, user.age);
+  }
+
+  printUser(kim);
 })();
