f81a831c63129aef36974c57e42eec46d42a67e1
hungerbk <wjantlr@gmail.com>
2023-11-01 21:56:15 +0900

    타입 별칭으로 객체의 타입 선언

--- workspace/ch01/ex01-07.ts
+++ workspace/ch01/ex01-07.ts
@@ -1,4 +1,19 @@
 // 객체 타입 선언
-(()=>{
-  
+(() => {
+  // type alias 객체의 타입 선언
+  type User = {
+    name: string;
+    age: number;
+  };
+  // 객체 생성
+  var kim: User = { name: "김철수", age: 30 };
+
+  console.log(kim);
+
+  function printUser(user: User): void {
+    // void이기 때문에 함수의 리턴값을 변수에 할당할 수 없음 (리턴값이 없음)
+    console.log(user.name, user.age);
+  }
+
+  printUser(kim);
 })();
