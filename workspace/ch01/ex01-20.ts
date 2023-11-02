621c793db15ed58aaaf336831fb7a587580af9e1
hungerbk <wjantlr@gmail.com>
2023-11-02 13:10:24 +0900

    숫자형 Enum 사용

--- workspace/ch01/ex01-20.ts
+++ workspace/ch01/ex01-20.ts
@@ -1,1 +1,23 @@
 // ex01-19.ts 복사
+(() => {
+  // 상수 데이터들의 집합을 정의
+  enum ClassName {
+    JS = 100,
+    TS = 111,
+    REACT = 222,
+  }
+  function getSchedule(subject: ClassName): string | undefined {
+    if (ClassName.JS == subject) {
+      return `JavaScript 수업은 4일 교육 + 2일 프로젝트입니다.`;
+    } else if (ClassName.TS === subject) {
+      return `TypeScript 수업은 3일 교육 + 1일 프로젝트입니다.`;
+    }
+  }
+
+  const jsSchedule = getSchedule(ClassName.JS);
+  const tsSchedule = getSchedule(ClassName.TS);
+
+  console.log(jsSchedule, tsSchedule);
+
+  console.log(ClassName);
+})();
