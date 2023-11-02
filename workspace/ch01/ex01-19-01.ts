8f98dfd6c74a674d13b5eceb0f96b02f041cb02c
hungerbk <wjantlr@gmail.com>
2023-11-02 13:10:06 +0900

    상수를 그룹화

--- workspace/ch01/ex01-19-01.ts
+++ workspace/ch01/ex01-19-01.ts
@@ -1,1 +1,28 @@
 // ex01-19.ts 복사
+(() => {
+  type ReadOnly = {
+    // readonly JS: string;
+    // readonly TS: string;
+    // readonly REACT: string;
+    readonly [lang: string]: string;
+  };
+
+  const ClassName: ReadOnly = {
+    JS: "JavaScript",
+    TS: "Typescript",
+    REACT: "React",
+  };
+
+  function getSchedule(subject: string): string | undefined {
+    if (ClassName.JS == subject) {
+      return `JavaScript 수업은 4일 교육 + 2일 프로젝트입니다.`;
+    } else if (ClassName.TS === subject) {
+      return `TypeScript 수업은 3일 교육 + 1일 프로젝트입니다.`;
+    }
+  }
+
+  const jsSchedule = getSchedule(ClassName.JS);
+  const tsSchedule = getSchedule(ClassName.TS);
+
+  console.log(jsSchedule, tsSchedule);
+})();
