e78cc6e62d1f0329428e0d15004d6f2759103cc6
hungerbk <wjantlr@gmail.com>
2023-11-02 13:10:37 +0900

    문자형 Enum 사용

--- workspace/ch01/ex01-21.ts
+++ workspace/ch01/ex01-21.ts
@@ -1,1 +1,24 @@
 // ex01-20.ts 복사
+(() => {
+  // 상수 데이터들의 집합을 정의
+  // JS에서는 ClassName이라는 객체를 생성
+  enum ClassName {
+    JS = "JavsScript",
+    TS = "TypeScript",
+    REACT = "Reacts",
+  }
+  function getSchedule(subject: ClassName): string | undefined {
+    if (ClassName.JS == subject) {
+      return `${ClassName.JS} 수업은 4일 교육 + 2일 프로젝트입니다.`;
+    } else if (ClassName.TS === subject) {
+      return `${ClassName.TS} 수업은 3일 교육 + 1일 프로젝트입니다.`;
+    }
+  }
+
+  const jsSchedule = getSchedule(ClassName.JS);
+  const tsSchedule = getSchedule(ClassName.TS);
+
+  console.log(jsSchedule, tsSchedule);
+
+  console.log(ClassName);
+})();
