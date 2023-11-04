dd2c7ebed70269c8a102d0a57ac3501f6b1f6ea4
hungerbk <wjantlr@gmail.com>
2023-11-04 23:37:16 +0900

    03-06 유니언 타입을 기반으로 객체 타입 생성(in)

--- workspace/ch03/ex03-06.ts
+++ workspace/ch03/ex03-06.ts
@@ -1,3 +1,19 @@
-(()=>{
-  
+// Mapped type
+// 제네릭 타입과 맵드 타입을 잘 이용하면 유틸리티 타입을 만들 수 있다
+(() => {
+  type UserField = "id" | "name" | "address" | "phone";
+
+  // userfield 안에 있는 값으로 새로운 타입 생성. (모드 스트링으로)
+  type User = {
+    [Field in UserField]: string;
+  };
+
+  const kim: User = {
+    id: "kim",
+    name: "김철수",
+    address: "서울시",
+    phone: "0100000",
+  };
+
+  console.log(kim);
 })();
