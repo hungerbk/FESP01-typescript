7f3904771c89735d748dd27cda6944d89489fc95
hungerbk <wjantlr@gmail.com>
2023-11-04 23:57:59 +0900

    03-18 속성 값으로 체크

--- workspace/ch03/ex03-18.ts
+++ workspace/ch03/ex03-18.ts
@@ -1,3 +1,27 @@
-(()=>{
-  
+(() => {
+  const kim: User = {
+    name: "김철수",
+    age: 30,
+    nickname: "철수",
+  };
+
+  const lee: UnknownUser = {
+    name: "이영희",
+    age: "비공개",
+  };
+
+  const hong: AdminUser = {
+    name: "홍길동",
+  };
+
+  function printUser(user: User | UnknownUser | AdminUser) {
+    // User 타입일 경우 나이 출력
+    if ("age" in user && typeof user.age === "number" /* 이렇게 작성(복잡하게)해도 타입가드가 제대로 작동하지 않을 수 있음 */) {
+      console.log((user as User).nickname);
+    }
+  }
+
+  printUser(kim);
+  printUser(lee);
+  printUser(hong);
 })();
